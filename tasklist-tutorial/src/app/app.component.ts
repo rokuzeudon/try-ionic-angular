7e0e078ba72c17ff47d12055b5302eb6efdc8daa
rokuzeudon <g-awaya@6q.design>
2020-02-01 17:46:36 +0900

    section12: ライフサイクルを理解する

--- tasklist-tutorial/src/app/app.component.ts
+++ tasklist-tutorial/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
@@ -9,7 +9,7 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy{
   public appPages = [
     {
       title: 'Home',
