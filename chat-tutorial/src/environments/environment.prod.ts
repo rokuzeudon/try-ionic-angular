fee1b0113016837a8078f675c9d053916ad0a6e5
rokuzeudon <g-awaya@6q.design>
2020-02-02 00:38:31 +0900

    section21: step1 Firebaseアカウント開設とIonicの設定

--- chat-tutorial/src/environments/environment.prod.ts
+++ chat-tutorial/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCIJzAEBP6pJ17PrJd3fzffu-tI4PvLV94',
+    authDomain: 'ionic-chat-8bb26.firebaseapp.com',
+    databaseURL: 'https://ionic-chat-8bb26.firebaseio.com',
+    projectId: 'ionic-chat-8bb26',
+    storageBucket: 'ionic-chat-8bb26.appspot.com',
+    messagingSenderId: '20146206415',
+    appId: '1:20146206415:web:748bed58240d880d95a9ba'
+  }
 };
--- chat-tutorial/src/environments/environment.ts
+++ chat-tutorial/src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCIJzAEBP6pJ17PrJd3fzffu-tI4PvLV94',
+    authDomain: 'ionic-chat-8bb26.firebaseapp.com',
+    databaseURL: 'https://ionic-chat-8bb26.firebaseio.com',
+    projectId: 'ionic-chat-8bb26',
+    storageBucket: 'ionic-chat-8bb26.appspot.com',
+    messagingSenderId: '20146206415',
+    appId: '1:20146206415:web:748bed58240d880d95a9ba'
+  }
 };
 
 /*
