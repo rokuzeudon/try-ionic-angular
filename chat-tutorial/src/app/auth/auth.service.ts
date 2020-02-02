cc63ae0468fbac80fa00a07899453010c36b5652
rokuzeudon <g-awaya@6q.design>
2020-02-02 09:46:42 +0900

    section21: step3 認証機能の実装 認証エラーのハンドリング

--- chat-tutorial/src/app/auth/auth.service.ts
+++ chat-tutorial/src/app/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
-import { NavController } from '@ionic/angular';
+import { NavController, AlertController } from '@ionic/angular';
+import { firebaseError } from '../auth/firebase.error';
 
 @Injectable({
   providedIn: 'root'
@@ -10,13 +11,15 @@ export class AuthService {
   constructor(
     public afAuth: AngularFireAuth,
     public navController: NavController,
+    public alertController: AlertController,
   ) { }
 
   authSignUp(login: { email: string, password: string }) {
     return this.afAuth.auth
       .createUserWithEmailAndPassword(login.email, login.password)
       .then(() => this.navController.navigateForward('/'))
-      .catch(error => {
+      .catch(async error => {
+        this.alertError(error);
         throw error;
       });
   }
@@ -25,7 +28,8 @@ export class AuthService {
     return this.afAuth.auth
       .signInWithEmailAndPassword(login.email, login.password)
       .then(() => this.navController.navigateForward('/'))
-      .catch(error => {
+      .catch(async error => {
+        this.alertError(error);
         throw error;
       });
   }
@@ -33,8 +37,21 @@ export class AuthService {
   authSignOut() {
     return this.afAuth.auth.signOut()
       .then(() => this.navController.navigateRoot('/auth/signin'))
-      .catch(error => {
+      .catch(async error => {
+        this.alertError(error);
         throw error;
       });
+  }
+
+  async alertError(e) {
+    if (firebaseError.hasOwnProperty(e.code)) {
+      e = firebaseError[e.code];
+    }
+    const alert = await this.alertController.create({
+      header: e.code,
+      message: e.message,
+      buttons: ['閉じる'],
+    });
+    await alert.present();
   }
 }
