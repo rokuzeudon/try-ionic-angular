2ec7a0681f72bb3e28d4c75a8be431d7ee3a7ea7
rokuzeudon <g-awaya@6q.design>
2020-02-02 11:25:42 +0900

    section21: step4 プロフ登録のための設定とモックアップ作成 カメラ起動など

--- chat-tutorial/src/app/shared/profile/profile.page.ts
+++ chat-tutorial/src/app/shared/profile/profile.page.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { Plugins, CameraResultType } from '@capacitor/core';
 
 @Component({
-  selector: 'app-profile',
-  templateUrl: './profile.page.html',
-  styleUrls: ['./profile.page.scss'],
+  selector: "app-profile",
+  templateUrl: "./profile.page.html",
+  styleUrls: ["./profile.page.scss"]
 })
 export class ProfilePage implements OnInit {
+  uid: string;
+  user = {
+    displayName: null,
+    photoDataUrl: null
+  };
+  photo: string;
 
-  constructor() { }
+  constructor(public modalController: ModalController) {}
 
-  ngOnInit() {
+  ngOnInit() { }
+
+  modalDismiss() {
+    this.modalController.dismiss();
   }
 
+  async takePicture() {
+    const image = await Plugins.Camera.getPhoto({
+      quality: 100,
+      resultType: CameraResultType.DataUrl,
+    });
+    this.photo = image && image.dataUrl;
+  }
 }
